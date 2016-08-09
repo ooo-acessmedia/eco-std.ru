b1cd2fa215ca1aea6556bc7065d170e5e584485f
mapuaptu <mapuaptu@gmail.com>
2016-08-09 11:26:42 +0400

    start repository

---
+++ assets/templates/js/main.js
@@ -0,0 +1,129 @@
+(function ($) {
+
+    'use strict';
+
+    // var cl  = function(msg){
+    //     console.log(msg);
+    // };
+
+    // Sliders
+
+    $('.product-page-slider').owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        loop: true,
+        nav: true,
+        navText: ['', '']
+    });
+
+    // Fancybox
+
+    $('.fancybox').fancybox();
+
+    // Адативная всплывающая форма
+
+    var $activateButton = $('.site-callout'),
+        $formPopup = $('.form-popup'),
+        $formFade = $('.form-fade'),
+        $formClose = $('.form-popup-close'),
+        $formWrap = $('.form-popup-wrap'),
+        thisPlaceholder,
+        fadeTimeout = 300;
+
+    var activatePopupForm = function (activateButton, formPopup, formFade, formWrap) {
+        activateButton.on('click', function () {
+            formPopup.add(formFade).addClass('is-visible form-fade-in');
+            formWrap.addClass('is-flex');
+            setTimeout(function () {
+                formPopup.add(formFade).removeClass('form-fade-in');
+            }, fadeTimeout);
+        });
+
+        formFade.add($formClose).on('click', function () {
+            formPopup.add(formFade).addClass('form-fade-out');
+
+            setTimeout(function () {
+                formPopup.add(formFade).removeClass('is-visible form-fade-out');
+                formWrap.removeClass('is-flex');
+            }, fadeTimeout);
+        });
+    };
+
+    activatePopupForm($activateButton, $formPopup, $formFade, $formWrap);
+
+    // Сменяющиеся плейсхолдеры для формы
+
+    $formPopup.find('input').add($formPopup.add('textarea'))
+        .focus(function () {
+            thisPlaceholder = $(this).attr('placeholder');
+            $(this).data('placeholder', thisPlaceholder);
+            $(this).attr('placeholder', '');
+        })
+        .blur(function () {
+            thisPlaceholder = $(this).data('placeholder');
+            $(this).attr('placeholder', thisPlaceholder);
+        });
+
+    // Значения даты для формы
+
+    var currentDate = new Date();
+
+    currentDate.setDate(currentDate.getDate() + 1);
+
+    var currentDay = currentDate.getDate();
+    var currentYear = currentDate.getFullYear();
+    var currentMonth = currentDate.getMonth();
+    $('#day').val(currentDay);
+    $('#year').val(currentYear);
+    $('#month').find('option').eq(currentMonth).attr('selected', 'selected');
+
+    // Линия вложенности у левого меню
+
+    var $leftMenuActiveUl = $('.left-menu .level-01.active > ul'),
+        $lastElement = $leftMenuActiveUl.find('> li').last();
+
+    if ($lastElement.position()) {
+        var leftMenuLineHeight = $lastElement.position().top + $lastElement.height() / 2;
+        var $leftMenuLine = $('<div>').attr('class', 'left-menu-line').css('height', leftMenuLineHeight);
+    }
+
+    $leftMenuActiveUl.append($leftMenuLine);
+    
+    function setTitleHeight(row) {
+        var height = 0;
+        row.forEach(function (item) {
+            var h = $(item).height();
+            if(h > height) height = h;
+        });
+        height += 36;
+        row.forEach(function (item) {
+            var h = $(item).height();
+            var p = (height + 18 - h)/2;
+            $(item).css({
+                'height': (height + 18) + 'px',
+                'padding-top': (p - 18) + 'px',
+                'padding-bottom': p + 'px',
+                'box-sizing': 'border-box'
+            });
+        });
+    }
+
+    var itemRow = [];
+
+    $('.category-item').each(function (i, item) {
+        itemRow.push($(item).find('.title'));
+        if((i + 1) % 2 == 0) {
+            setTitleHeight(itemRow);
+            itemRow = [];
+        }
+    });
+    setTitleHeight(itemRow);
+
+
+
+
+})(jQuery);
+
+
+
---
+++ gruntfile.js
@@ -0,0 +1,185 @@
+'use strict';
+module.exports = function (grunt) {
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+
+    var deployServer = '31.31.196.102',
+        deployFolder = '',
+        liveReloadFolder = 'http://eco-std.ru/';
+
+    grunt.initConfig({
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/templates/css',
+                    src: ['assets/templates/main-new.css'],
+                    dest: 'assets/templates/main-new.css',
+                    ext: '.css'
+                }]
+            },
+            options: {
+                keepSpecialComments: 0
+            }
+        },
+
+        less: {
+            develop: {
+                options: {
+                    paths: ['assets/templates'],
+                    relativeUrls: true,
+                    modifyVars: {}
+                },
+                files: {
+                    'assets/templates/css/main-new.css': 'assets/templates/less/main.less'
+                }
+            },
+
+            production: {
+                options: {
+                    paths: ['assets/templates'],
+                    relativeUrls: true,
+                    plugins: [
+                        (new (require('less-plugin-clean-css'))({}))
+                    ],
+                    modifyVars: {}
+                },
+                files: {
+                    'assets/templates/css/main-new.css': 'assets/templates/less/main.less'
+                }
+            }
+        },
+
+        'ftp-deploy': {
+            css: {
+                auth: {
+                    host: deployServer,
+                    port: 21,
+                    authKey: 'key1'
+                },
+                src: 'assets/templates/css/',
+                dest: 'www/eco-std.ru/assets/templates/css/',
+                exclusions: ['assets/templates/css/owl.min.less', 'assets/templates/css/fancybox.min.less',
+                    'assets/templates/css/bootstrap.min.less', 'assets/templates/css/boilerplate.min.less']
+            },
+
+            js: {
+                auth: {
+                    host: deployServer,
+                    port: 21,
+                    authKey: 'key1'
+                },
+                src: 'assets/templates/js/',
+                dest: 'www/eco-std.ru/assets/templates/js/',
+                exclusions: ['jquery.fancybox.min.js', 'jquery-1.12.0.min.js', 'owl.carousel.min.js']
+
+            },
+
+            images: {
+                auth: {
+                    host: deployServer,
+                    port: 21,
+                    authKey: 'key1'
+                },
+                src: 'assets/templates/img/',
+                dest: 'www/eco-std.ru/assets/templates/img/'
+            },
+
+            'js-minishop': {
+                auth: {
+                    host: deployServer,
+                    port: 21,
+                    authKey: 'key1'
+                },
+                src: 'assets/components/minishop2/js/web/',
+                dest: 'www/eco-std.ru/assets/components/minishop2/js/web/'
+            }
+
+        },
+
+        uglify: {
+            production: {
+                files: {
+                    'assets/templates/js/main.min.js': ['assets/templates/js/main.js'],
+                    'assets/templates/js/ajaxform.js': ['assets/templates/js/ajaxform.js'],
+                    'assets/components/minishop2/js/web/default.js': 'assets/components/minishop2/js/web/default.js'
+                }
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                options: {
+                    optimizationLevel: 3
+                },
+
+                files: [{
+                    expand: true,
+                    cwd: 'assets/templates/img',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'assets/templates/img'
+
+                }]
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['last 3 versions', 'ie 10']
+            },
+            dist: {
+                files: {
+                    'assets/templates/css/main-new.css': 'assets/templates/css/main-new.css'
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['assets/templates/less/**/*.less'],
+                tasks: ['less:develop', 'autoprefixer', 'ftp-deploy:css'],
+                options: {
+                    spawn: false,
+                    livereload: {
+                        host: liveReloadFolder
+                    }
+                }
+            },
+            js: {
+                files: ['assets/templates/js/main.js'],
+                tasks: ['ftp-deploy:js'],
+                options: {
+                    spawn: false,
+                    livereload: {
+                        host: liveReloadFolder
+                    }
+                }
+            },
+
+            'grunt-js': {
+                files: ['gruntfile.js'],
+                tasks: ['watch'],
+                options: {
+                    spawn: false,
+                    livereload: {
+                        host: liveReloadFolder
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['watch']);
+
+    // Production
+    grunt.registerTask('production', ['uglify:production', 'ftp-deploy:js', 'less:production', 'ftp-deploy:css',
+        'imagemin', 'ftp-deploy:images']);
+
+    // Production without images
+    grunt.registerTask('production-wimages', ['uglify:production', 'ftp-deploy:js', 'less:production', 'ftp-deploy:css']);
+
+    // Production for minishop2
+    grunt.registerTask('production-minishop', ['uglify:production', 'ftp-deploy:js', 'ftp-deploy:js-minishop',
+        'less:production', 'ftp-deploy:css', 'imagemin', 'ftp-deploy:images']);
+
+};
